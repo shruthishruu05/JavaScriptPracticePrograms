eb3aae8ade7ee3ec271af678cf9dc8059aa527a1
shruthishruu05 <shruthi.m@perfios.com>
2021-10-05 21:45:35 +0530

    REFACTORED : used command line input

--- IfAndElse/DayAndMonth.js
+++ IfAndElse/DayAndMonth.js
@@ -1,10 +1,17 @@
-var month = 6;
-var day = 30;
-if(month >=3 && month <= 6 && day <=31)
-{
-    console.log("TRUE");
-}
-else
-{
-    console.log("FALSE");
-}
+var readline = require('readline'); 
+var rl = readline.createInterface(
+     process.stdin, process.stdout);
+     
+var day;
+var month;
+
+rl.question('Enter day ', (dayInput) => {
+    day = dayInput;
+    rl.question('Enter month ', (monthInput) => {
+        month = monthInput;
+        if(month>=3 && month<=6){
+            console.log("True");
+        }else
+            console.log("False");
+    });
+    });
