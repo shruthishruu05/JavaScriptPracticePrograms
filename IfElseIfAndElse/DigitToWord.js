741e540c270481b591834808d6fae85d262295bb
shruthishruu05 <shruthi.m@perfios.com>
2021-10-05 22:10:51 +0530

    ADDED : class to write digit into word

---
+++ IfElseIfAndElse/DigitToWord.js
@@ -0,0 +1,40 @@
+
+function printValue(digit)
+{
+    if(digit ==0)
+    console.log("zero");
+    else if(digit ==1)
+    console.log("one");
+    else if(digit ==2)
+    console.log("two");
+    else if(digit ==3)
+    console.log("three");
+    else if(digit ==4)
+    console.log("four");
+    else if(digit ==5)
+    console.log("five");
+    else if(digit ==6)
+    console.log("six");
+    else if(digit ==7)
+    console.log("seven");
+    else if(digit ==8)
+    console.log("eight");
+    else if(digit ==7)
+    console.log("seven");
+    else if(digit ==8)
+    console.log("eight");
+    else
+    console.log("nine");
+}
+function printWord( number)
+{
+    var i;
+    var length = number.length;
+    for(i =0;i<length;i++)
+    {
+        printValue(number.charAt(i));
+    }
+}
+
+let number = "9";
+printWord(number);
